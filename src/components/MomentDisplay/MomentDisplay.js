21b4507476e0fdec11ce517d2a2ffb518dd57702
elisa <elisalenotre6@gmail.com>
2025-07-31 19:38:16 +0200

    changement dans moment display

--- src/components/MomentDisplay/MomentDisplay.js
+++ src/components/MomentDisplay/MomentDisplay.js
@@ -8,9 +8,9 @@ export default function MomentDisplay({moment, onEdit}) {
     <div className="moment-card">
       <h3>Trucs du jour</h3>
       
-      <p><a href={moment.music} target="_blank" rel="noreferrer">Musique du jour</a></p>
+      <p><strong>Musique du jour :</strong></p><p><a href={moment.music} target="_blank" rel="noreferrer">Cliques ici pour écouter</a></p>
       <p><strong>Film ou Série :</strong> {moment.movie}</p>
-      {moment.trinketImg && <img className="trinket-img" src={moment.trinketImg} alt="trinket"/>}
+      <p><strong>Ambiance du jour :</strong></p>{moment.trinketImg && <img className="trinket-img" src={moment.trinketImg} alt="trinket"/>}
       <p><strong>Activité fun :</strong> {moment.activity}</p>
       <p><strong>Repas :</strong> {moment.meal}</p>
       <p><strong>Soin :</strong> {moment.care}</p>
